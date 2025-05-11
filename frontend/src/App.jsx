a647645168f7975da562c656b1c3dc029f988767
RafaelRSCaldeira <rafael.caldeira.2005@gmail.com>
2025-05-10 22:52:54 -0300

    added router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import Home from './components/Home';
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
